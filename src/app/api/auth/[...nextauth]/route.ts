1a6bb27ec182af2e552f0c43ccc050cb87f5e636
Fernando Soliva <fernando@ubuntutech.com.br>
2024-11-25 20:38:01 -0300

    fix lint errors

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -96,7 +96,7 @@ const handler = NextAuth({
                     }
                 })
                 if (existingUser) {
-                    await saveUserProfile(existingUser.id, 'photographer', user.image)
+                    await saveUserProfile(existingUser.id, 'photographer', user.image!)
                     return true
                 }
 
